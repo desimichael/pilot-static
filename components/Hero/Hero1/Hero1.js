0ab51e3d5f7c1b44ba89ecc9fdfa30c966e24e22
Desi Steib <desiwebdev11@gmail.com>
2021-09-03 10:23:06 -0400

    All static files.

--- components/Hero/Hero1/Hero1.js
+++ components/Hero/Hero1/Hero1.js
@@ -4,7 +4,7 @@ export default function Hero1() {
       <img src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" className="hero1__bg" />
       <div className="hero1__content-container">
         <div className="hero1__info">
-          <h1 className="hero1__title">Pilot</h1>
+          <h1 className="hero1__title">Pilots</h1>
           <p className="hero1__paragraph">
             Hero1
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam exercitationem quia blanditiis assumenda voluptatum sequi iusto ducimus vero omnis quaerat?
