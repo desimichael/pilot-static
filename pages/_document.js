80e10dbf2fd9a2fecc5228034b9ba812d11f04af
Desi Steib <desiwebdev11@gmail.com>
2021-09-11 00:12:00 -0400

    added correct fonts to files and exported into static files

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,300;0,500;0,700;1,400&family=Lobster+Two&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;700;900&family=Lobster+Two:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
