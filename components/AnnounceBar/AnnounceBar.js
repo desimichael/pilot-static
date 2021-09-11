226afa9702394d048cf0c2545342aa0ea4e6f1f1
Desi Steib <desiwebdev11@gmail.com>
2021-09-11 18:23:32 -0400

    Added announce bar text option to section

--- components/AnnounceBar/AnnounceBar.js
+++ components/AnnounceBar/AnnounceBar.js
@@ -1,7 +1,9 @@
 export default function AnnounceBar() {
   return (
     <div className="announce-bar">
-      50% OFF Labor Day Weekend Sale
+      <p className="announce-bar__text">
+        50% OFF Labor Day Weekend Sale
+      </p>
     </div>
   )
 }
