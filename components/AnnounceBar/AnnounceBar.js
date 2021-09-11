a1382761192b8b86aa99c3df4ed538481471c321
Desi Steib <desiwebdev11@gmail.com>
2021-09-11 18:39:09 -0400

    Announce bar text option deleted.

--- components/AnnounceBar/AnnounceBar.js
+++ components/AnnounceBar/AnnounceBar.js
@@ -1,9 +1,7 @@
 export default function AnnounceBar() {
   return (
     <div className="announce-bar">
-      <p className="announce-bar__text">
-        50% OFF Labor Day Weekend Sale
-      </p>
+      50% OFF Labor Day Weekend Sale
     </div>
   )
 }
